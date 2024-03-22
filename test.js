0b497163aed4b9f2cc2f16576b7703171c245638
Susytest <131393493+Susytest@users.noreply.github.com>
2024-03-22 09:09:39 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-// test.js
-
-// Display a prompt dialog asking for the user's name
-let userName = prompt("Please enter your name", "Your Name");
-
-// Check if the user provided input
-if (userName) {
-    alert("Welcome, " + userName + "!");
-} else {
-    alert("You didn't enter a name. Please try again.");
-}
+window.onload = function() {
+    alert("Hello, this is an alert!");
+};
